743e16124004c29f29e3021615cea07dad9e75c3
Mohammadreza Azimi <79572023+mazimiucc@users.noreply.github.com>
2022-05-23 17:58:00 +0100

    Update readdata.py

--- readdata.py
+++ readdata.py
@@ -1,8 +1,12 @@
 import pandas as pd
 
-data = pd.read_csv('jadid2.txt', sep=",", header=None)
+data = pd.read_csv('1.txt', sep=",", header=None)
 data.columns = ["A", "M", "S", "L_e","N_t","P_r","N_b","B_i","B_ii","Nu"]
 
-print(data.describe())
+
+for i, item in  enumerate(data['Nu']):
+    data['Nu'].iloc[i] = float(str(item).replace("HFloat("," ").replace(")",""))
+
+print(data.head())
 
 data.to_csv('1.csv', sep=',')
